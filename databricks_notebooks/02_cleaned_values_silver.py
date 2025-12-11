8a6f2bb7ae5ec0ca0388dbee399abed496cec1f5
Abdullah Mohamed Ezzat <abdollah.mohamed2005@gmail.com>
2025-12-11 14:13:03 +0200

    Add files via upload

---
+++ databricks_notebooks/02_cleaned_values_silver.py
@@ -0,0 +1,40 @@
+#Imports
+from pyspark.sql.functions import *
+
+#configuraions
+spark.conf.set(
+  "fs.azure.account.key.storageecommercerealtime.dfs.core.windows.net",
+  "HDUWCmlNpT0atrALkow4RuIdCJ8IFp9VR0wQoc/XgQeXCM09gWcJZiU0wl0k6wKG8HZYU/WciMtv+AStCxz/qA=="
+)
+
+bronze_path = "abfss://ecommercecontainer@storageecommercerealtime.dfs.core.windows.net/bronze"
+
+silver_path = "abfss://ecommercecontainer@storageecommercerealtime.dfs.core.windows.net/silver"
+
+
+#Reading raw data from bronze
+df_bronze=(
+  spark.readStream
+  .format("delta")
+  .load(bronze_path)
+)
+
+#Clean and enrich
+df_clean=(
+  df_bronze
+  .withColumn("timestamp",to_timestamp("timestamp"))
+  .withColumn("price",when(col("price").isNull(),0.0).otherwise(col("price")))
+  .withColumn("quantity",when(col("quantity").isNull(),1).otherwise(col("quantity")))
+  .withColumn("total_amount",col("price")*col("quantity"))
+  .dropDuplicates(["order_id"])
+  .filter(col("country")=="USA")
+  .filter(col("state").isNotNull())
+)
+#Write to Silver Layer
+(
+  df_clean.writeStream
+ .format("delta")
+ .outputMode("append")
+ .option("checkpointLocation",silver_path+"/_checkpoint")
+ .start(silver_path)
+ )
